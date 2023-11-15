41b2df754dfb6730b3972804dd1fd9de2f952df2
yjb23tech <yjb23tech@gmail.com>
2023-11-15 14:38:27 +0000

    Hopefully this works

---
+++ FoodMenu.jsx
@@ -0,0 +1,16 @@
+const FoodMenu = () => {
+
+    return (
+
+        <div>
+            <h1>Suya Hut</h1>
+            <ul>
+                <li>Efo Riro</li>
+                <li>Obe</li>
+                <li>Pounded Yam</li>
+            </ul>
+        </div>
+    );
+};
+
+export default FoodMenu; 
